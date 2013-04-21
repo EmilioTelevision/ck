90bb8789968e4a1726473acd8c197eef9a09be11
James Gillmore <james@faceyspacey.com>
2013-04-20 18:39:14 -0700

    sdf

--- client/template_js~/ribbon.js
+++ client/template_js~/ribbon.js
@@ -1,5 +1,21 @@
 Template.ribbon.events({
-	'click .announcements': function() {
+	'mouseenter .announcements': function() {
+		if($('#ribbon').is(':visible')) {
+			$('#ribbon').animate({
+				opacity: 0,
+				top: '-=500'
+			}, 500, 'easeOutExpo', function() {
+				$(this).hide();
+			});
+		}
+		else {
+			$('#ribbon').show().animate({
+				opacity: 1,
+				top: '+=500'
+			}, 500, 'easeOutExpo');
+		}
+	},
+	'mouseleave .announcements': function() {
 		if($('#ribbon').is(':visible')) {
 			$('#ribbon').animate({
 				opacity: 0,
